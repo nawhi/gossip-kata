15e48ea0dc5d8147ae391f068c62fdaba051b6bf
Nick White <nick.white@triptease.com>
2020-02-20 21:35:18 +0000

    Start picking apart variable names Pick apart some variable names

--- src/GossipCalculator.ts
+++ src/GossipCalculator.ts
@@ -3,12 +3,16 @@ import { Route } from './Route';
 
 const MAX_STOPS = 480;
 
+function getRoutesFrom(rawRoutes: number[][]) {
+  return rawRoutes.map(stops => new Route(stops));
+}
+
 export function gossipCalculator(input: number[][]) {
-  const routes = input.map(stops => new Route(stops));
-  const tracker = new GossipTracker(routes.length);
-  const driverCombinations = generateDriverCombinations(routes.length);
+  const routes = getRoutesFrom(input);
+  const numDrivers = routes.length;
+  const tracker = new GossipTracker(numDrivers);
   for (let stopIx = 0; stopIx < MAX_STOPS; stopIx++) {
-    for (const [driver1, driver2] of driverCombinations) {
+    for (const [driver1, driver2] of generateDriverCombinations(numDrivers)) {
       const route1 = routes[driver1];
       const route2 = routes[driver2];
 
