74ab70f6b64aa38ad5839473506680a13c273bb3
Nick White <nick.white@triptease.com>
2020-02-20 11:43:18 +0000

    Calculate for 2 drivers who meet after 300 stops

--- src/GossipCalculator.test.ts
+++ src/GossipCalculator.test.ts
@@ -5,34 +5,49 @@ describe('GossipCalculator', () => {
     const routes = [[1], [1]];
     expect(new GossipCalculator(routes).calculateStops()).toBe(1);
   });
-  
+
   it('calculates that 2 drivers never meet if their routes do not interact', () => {
     const routes = [[1], [2]];
     expect(new GossipCalculator(routes).calculateStops()).toBe('never');
   });
-  
+
   it('calculates for 2 drivers who meet at the second stop', () => {
     const routes = [[1, 2], [3, 2]];
     expect(new GossipCalculator(routes).calculateStops()).toBe(2);
   });
-  
+
   it('calculates for 2 drivers who meet at the third stop', () => {
     const routes = [[1, 2, 3], [5, 4, 3]];
     expect(new GossipCalculator(routes).calculateStops()).toBe(3);
-  })
-  
+  });
+
+  it('calculates for 2 drivers who meet after 300 stops', () => {
+    const routes = [
+      Array(299).fill(1).concat(2),
+      Array(300).fill(2)
+    ];
+    expect(new GossipCalculator(routes).calculateStops()).toBe(300);
+  });
+
   it('repeats single-stop route when it ends', () => {
     const routes = [[1, 2, 3], [3]];
     expect(new GossipCalculator(routes).calculateStops()).toBe(3);
   });
-  
+
   it('repeats multi-stop route when it ends', () => {
     // 121212
     // 432432
     //      ^
     const routes = [[1, 2], [4, 3, 2]];
     expect(new GossipCalculator(routes).calculateStops()).toBe(6);
-  })
-  
-  it.todo('better for repeating route - [1, 2] + [4, 3, 2]')
+  });
+
+  xit('does not match drivers after the 480-stop maximum', () => {
+    const routes = [
+      Array(499).fill(1).concat(2),
+      Array(500).fill(2)
+    ];
+    expect(new GossipCalculator(routes).calculateStops()).toBe('never');
+  });
+
 });
--- src/GossipCalculator.ts
+++ src/GossipCalculator.ts
@@ -8,11 +8,12 @@ export class GossipCalculator {
   public calculateStops(): number | 'never' {
     const [r1, r2] = this.routes;
 
-    for (const i of [0, 1, 2, 3, 4, 5]) {
+    for (let i = 0; i < 301; i++) {
       if (r1[i % r1.length] === r2[i % r2.length]) {
         return i + 1;
       }
     }
+
     return 'never';
   }
 }
