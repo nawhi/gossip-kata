417f1c5fa616dba3cdf14094c84c0448e56c547b
Nick White <nick.white@triptease.com>
2020-02-20 11:24:20 +0000

    Check drivers which meet at third stop (naive impl)

--- src/GossipCalculator.test.ts
+++ src/GossipCalculator.test.ts
@@ -11,8 +11,20 @@ describe('GossipCalculator', () => {
     expect(new GossipCalculator(routes).calculateStops()).toBe('never');
   });
   
-  it('calculates that 2 drivers with different routes meet at the second stop', () => {
+  it('calculates for 2 drivers who meet at the second stop', () => {
     const routes = [[1, 2], [3, 2]];
     expect(new GossipCalculator(routes).calculateStops()).toBe(2);
+  });
+  
+  it('calculates for 2 drivers who meet at the third stop', () => {
+    const routes = [[1, 2, 3], [5, 4, 3]];
+    expect(new GossipCalculator(routes).calculateStops()).toBe(3);
   })
+  
+  xit('repeats routes when they end', () => {
+    const routes = [[1, 2, 3], [3]];
+    expect(new GossipCalculator(routes).calculateStops()).toBe(3);
+  });
+  
+  it.todo('better for repeating route - [1, 2] + [4, 3, 2]')
 });
--- src/GossipCalculator.ts
+++ src/GossipCalculator.ts
@@ -2,19 +2,25 @@ type Stop = number;
 type Route = Stop[];
 
 export class GossipCalculator {
-    constructor(private routes: Route[]) {
+  constructor(private routes: Route[]) {
 
+    // 12121212
+    // 432432432
+
+  }
+
+  public calculateStops(): number | 'never' {
+    const [r1, r2] = this.routes;
+
+    if (r1[0] === r2[0]) {
+      return 1;
     }
-
-    public calculateStops(): number | 'never' {
-        const [r1, r2] = this.routes;
-
-        if (r1[0] === r2[0]) {
-            return 1;
-        }
-        if (Number(r1[1]) === Number(r2[1])) {
-            return 2;
-        }
-        return 'never';
+    if (Number(r1[1]) === Number(r2[1])) {
+      return 2;
     }
+    if (Number(r1[2]) === Number(r2[2])) {
+      return 3;
+    }
+    return 'never';
+  }
 }
