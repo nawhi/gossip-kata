bd15443108986c8e5b430fd8026efee5c0b00434
Nick White <nick.white@triptease.com>
2020-02-20 21:33:26 +0000

    Change GossipCalculator class altogether to just a function

--- src/GossipCalculator.ts
+++ src/GossipCalculator.ts
@@ -3,34 +3,23 @@ import { Route } from './Route';
 
 const MAX_STOPS = 480;
 
-export function calculateStops(routes: number[][]) {
-  return new GossipCalculator(routes).calculateStops();
-}
+export function calculateStops(input: number[][]) {
+  const routes = input.map(stops => new Route(stops));
+  const tracker = new GossipTracker(routes.length);
+  const driverCombinations = generateDriverCombinations(routes.length);
+  for (let stopIx = 0; stopIx < MAX_STOPS; stopIx++) {
+    for (const [driver1, driver2] of driverCombinations) {
+      const route1 = routes[driver1];
+      const route2 = routes[driver2];
 
-class GossipCalculator {
-  private readonly routes: Route[];
-
-  constructor(input: number[][]) {
-    this.routes = input.map(stops => new Route(stops));
-  }
-
-  public calculateStops(): number | 'never' {
-    const tracker = new GossipTracker(this.routes.length);
-    const driverCombinations = generateDriverCombinations(this.routes.length);
-    for (let stopIx = 0; stopIx < MAX_STOPS; stopIx++) {
-      for (const [driver1, driver2] of driverCombinations) {
-        const route1 = this.routes[driver1];
-        const route2 = this.routes[driver2];
-
-        if (route1.stopAt(stopIx) === route2.stopAt(stopIx)) {
-          tracker.exchangeGossip(driver1, driver2);
-        }
-      }
-      if (tracker.allGossipExchanged()) {
-        return stopIx + 1;
+      if (route1.stopAt(stopIx) === route2.stopAt(stopIx)) {
+        tracker.exchangeGossip(driver1, driver2);
       }
     }
-
-    return 'never';
+    if (tracker.allGossipExchanged()) {
+      return stopIx + 1;
+    }
   }
+
+  return 'never';
 }
